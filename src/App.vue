d3b4fa3c9e0375889020f6be0e19f6debe097e98
zxfjd3g <258147149@qq.com>
2018-05-09 13:44:51 +0800

    init demo app

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <TodoHeader :addTodo="addTodo"/>
+      <TodoList :todos="todos"/>
+      <TodoFooter/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader.vue'
+  import TodoList from './components/TodoList.vue'
+  import TodoFooter from './components/TodoFooter.vue'
+
+  export default {
+    data () {
+      return {
+        todos: [
+          {title: '吃饭', complete: false},
+          {title: '睡觉', complete: true},
+          {title: '玩手机', complete: false}
+        ]
+      }
+    },
+
+    methods: {
+      addTodo (todo) {
+        this.todos.unshift(todo)
+      }
+    },
+
+    components: {
+      TodoHeader,
+      TodoList,
+      TodoFooter
+    }
+  }
+</script>
+
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
