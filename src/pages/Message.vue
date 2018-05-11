aaea9d3e305d758f616cfa049fa32926a00f1af9
zxfjd3g <258147149@qq.com>
2018-05-11 16:20:24 +0800

    路由导航

--- src/pages/Message.vue
+++ src/pages/Message.vue
@@ -3,8 +3,12 @@
     <ul>
       <li v-for="m in messages" :key="m.id">
         <router-link :to="`/home/message/detail/${m.id}`">{{m.title}}</router-link>
+        <button @click="pushShow(m.id)">push查看</button>
+        <button @click="replaceShow(m.id)">replace查看</button>
       </li>
     </ul>
+    <button @click="$router.back()">回退</button>
+    <hr>
     <router-view></router-view>
   </div>
 </template>
@@ -30,6 +34,16 @@
         ]
         this.messages = messages
       }, 1000)
+    },
+
+    methods: {
+      pushShow (id) {
+        this.$router.push(`/home/message/detail/${id}`)
+      },
+
+      replaceShow(id) {
+        this.$router.replace(`/home/message/detail/${id}`)
+      }
     }
   }
 </script>
